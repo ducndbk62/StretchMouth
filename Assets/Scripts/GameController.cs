c1f3019cec2a0b60e0da7427d0f0234523df0167
Nguyễn Đình Đức <ducndbk62@gmail.com>
2021-08-15 18:46:43 +0700

    +first commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject gameMenu;
+    public GameObject gameFinish;
+    public GameObject gameover;
+    public GameObject gameShop;
+    public GameObject gameSettings;
+    public GameObject restartButton;
+
+    //public Text textLevelCoins;
+    //public Text textTotalCoins;
+
+    //private int levelCoins;
+    //private int totalCoins;
+
+    Scene scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+        scene = SceneManager.GetActiveScene();
+        //totalCoins = PlayerPrefs.GetInt("TotalCoins");
+        //textTotalCoins.text = totalCoins.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //textLevelCoins.text = levelCoins.ToString();
+    }
+
+    public void Play()
+    {
+        //textLevelCoins.gameObject.SetActive(true);
+        gameMenu.SetActive(false);
+        restartButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        //SetTotalCoins();
+        SceneManager.LoadScene(scene.buildIndex);
+        restartButton.SetActive(false);
+    }
+
+    public void NextLevel()
+    {
+        //SetTotalCoins();
+        int nextLevel = scene.buildIndex + 1;
+        SceneManager.LoadScene(nextLevel);
+    }
+
+
+
+    public void AddLevelCoins(int coin)
+    {
+        //levelCoins += coin;
+    }
+
+    //public void MultipleLevelCoins(int multiple)
+    //{
+    //    //levelCoins *= multiple;
+    //}
+
+    public void ToShop()
+    {
+        gameMenu.SetActive(false);
+        gameShop.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        gameShop.SetActive(false);
+        gameSettings.SetActive(false);
+        gameMenu.SetActive(true);
+    }
+
+    public void ToSettings()
+    {
+        gameMenu.SetActive(false);
+        gameSettings.SetActive(true);
+    }
+
+    //void SetTotalCoins()
+    //{
+    //    totalCoins += levelCoins;
+    //    PlayerPrefs.SetInt("TotalCoins", totalCoins);
+    //    textTotalCoins.text = totalCoins.ToString();
+    //}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float decreaseScale;
+
+    Vector3 direction;
+    Vector3 startScale;
+    float startMouseHigh;
+    const float maxScale = 2.5f;
+    const float minScale = 0.35f;
+    float lastScale = 1;
+    float scale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startScale = gameObject.transform.localScale;
+        direction = Vector3.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ControlMoving();
+        ControlScale();
+    }
+
+    void ControlMoving()
+    {
+        gameObject.transform.Translate(direction * Time.deltaTime * moveSpeed);
+    }
+
+    void ControlScale()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startMouseHigh = Input.mousePosition.y;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            scale = lastScale + (Input.mousePosition.y - startMouseHigh) / decreaseScale;
+            if (scale > maxScale) scale = maxScale;
+            if (scale < minScale) scale = minScale;
+            if (scale != 0)
+            {
+                gameObject.transform.localScale = new Vector3(startScale.x, startScale.y * scale, startScale.z / scale);
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            lastScale = scale;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Turn right")
+        {
+            gameObject.transform.Rotate(new Vector3(0, 90, 0));
+            //gameObject.transform.RotateAroundLocal(Vector3.up, 90);
+        }
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
